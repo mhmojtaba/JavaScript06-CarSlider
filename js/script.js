f7cd6d960d5dbceaa94a086f35fc16558706e42f
Mojtaba Mohammadi <mhmojtaba@gmail.com>
2022-10-31 22:49:26 +0330

    moving active class by Js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-function slider(item) {
-  document.querySelector("#image").src = item;
-}
+const li = document.getElementById("li").children;
+// console.log(li);
 
-function changBg(color) {
-  const bg = document.querySelector(".section");
-  bg.style.backgroundColor = color;
+for (let i = 0; i < li.length; i++) {
+  li[i].onclick = (e) => {
+    // console.log(e);
+    let n = 0;
+    while (n < li.length) {
+      li[n++].className = "";
+    }
+    // console.log(n);
+    e.target.className = "active";
+  };
 }
